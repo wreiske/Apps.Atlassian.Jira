91f23d9db41021bdf321f78ba8d61fb0bc9ef4a7
Douglas Gubert <douglas.gubert@gmail.com>
2018-10-30 15:41:39 -0300

    Add a helper that generates the full URL and auth token for Atlassian

--- helpers.ts
+++ helpers.ts
@@ -1,8 +1,10 @@
 import { IMessageBuilder, IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { RocketChatAssociationModel, RocketChatAssociationRecord } from '@rocket.chat/apps-engine/definition/metadata';
 import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
 import { IUser } from '@rocket.chat/apps-engine/definition/users';
 
 import { AppSetting } from './app-settings';
+import * as jwt from './jwt';
 
 export async function startNewMessageWithDefaultSenderConfig(modify: IModify, read: IRead, sender: IUser, room: IRoom): Promise<IMessageBuilder> {
     const settingsReader = read.getEnvironmentReader().getSettings();
@@ -20,4 +22,25 @@ export function parseJiraDomainFromIssueUrl(issueUrl: string): string {
     const [domain] = (issueUrl.match(/^https?:\/\/[^\/]+/) as Array<any>);
 
     return domain;
+}
+
+export async function getUrlAndAuthToken(read: IRead, path: string, method: string = 'GET'): Promise<{ url: string, token: string }> {
+    const association = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, 'auth');
+    const records = await read.getPersistenceReader().readByAssociation(association);
+    const authData: any = records[0];
+    const req: jwt.IRequest = jwt.fromMethodAndUrl(method, path);
+
+    const tokenData = {
+        iss: 'rocketchat',
+        iat: Date.now() / 1000,
+        exp: Date.now() / 1000 + 60 * 3,
+        qsh: jwt.createQueryStringHash(req),
+    };
+
+    const token = jwt.encode(tokenData, authData.sharedSecret);
+
+    return {
+        url: `${authData.baseUrl}${path}`,
+        token,
+    };
 }
